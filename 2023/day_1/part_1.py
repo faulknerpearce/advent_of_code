1dad3e2e88cf53260e28a2ea90d677a630e0e3b4
FaulknerPearce <awesomeaustin1998@yahoo.com.au>
2023-12-03 10:33:41 -0700

    solutions for part one and part 2 of day one.

--- 2023/day_1/part_1.py
+++ 2023/day_1/part_1.py
@@ -1,20 +1,24 @@
+# Read the contents of a file, split lines, and return as a list
 def read_file_to_list(file):
     with open(file) as text:
         text_lines = text.read().splitlines()
     return text_lines
 
+# Find and return the first numeric character in a string.
 def find_first_number(line):
     for index in range(len(line)):
         if ord(line[index]) >= 49 and ord(line[index]) <= 57:
             return line[index]
     return None 
 
+# Find and return the last numeric character in a string.
 def find_last_number(line):
     for index in range(1, len(line)+1):
-        if ord(line[-index]) >= 49 and ord(line[-index]) <=57:
+        if ord(line[-index]) >= 49 and ord(line[-index]) <= 57:
             return line[-index] 
     return None 
 
+# combine the first digit and the last digit to form a single two-digit number and return the sum.
 def part_one(lines):
     total = 0 
     for line in lines:
--- 2023/day_1/part_2.py
+++ 2023/day_1/part_2.py
@@ -1,58 +1,66 @@
-def read_file(file):
+# Read the contents of a file, split lines, and return as a list
+def read_file_return_list(file):
     with open(file) as text:
         text_lines = text.read().splitlines()
     return text_lines
 
+# Find and return the first numeric character in a string.
 def find_first_number(line):
     for index in range(len(line)):
         if ord(line[index]) >= 48 and ord(line[index]) <= 57:
             return line[index]
     return None 
 
+# Find and return the last numeric character in a string.
 def find_last_number(line):
     for index in range(1, len(line)+1):
         if ord(line[-index]) >= 48 and ord(line[-index]) <= 57:
             return line[-index]
     return None 
 
-def find_index(str_line, greater):
+# Count the occurrences of numeric words in a string.
+def count_occurrences(str_line):
+    numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    numbers_found = 0
+    for word in numbers:
+        numbers_found += str_line.count(word) 
+    return numbers_found
+
+# Replace numeric words with digits in a string.
+def replace_words_for_digits(str_line):
     numbers_map = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8,  'nine': 9}
-    index = None
-    for key in numbers_map.keys():
-        try:
-            found_index = str_line.index(key)
-            if greater == True: 
-                if index == None or found_index > index: 
-                    index = found_index
-                    word = key
-                    number = numbers_map[key]
-            else: 
-                if index == None or found_index < index: 
-                    index = found_index
-                    word = key
-                    number = numbers_map[key]
+    numbers_found = count_occurrences(str_line)
+    numbers_replaced = 0
 
-        except ValueError:
-            continue
-    if index == None:
-        return str_line
-    else:
-        forward_format = str_line.replace(str(word), str(number))
-        return forward_format
+    while numbers_replaced < numbers_found:
+        index = None
 
+        for key in numbers_map.keys():
+            try:
+                found_index = str_line.lower().index(key)
+                if index is None or found_index < index: 
+                    index = found_index
+                    word = key[:-1]
+                    number = numbers_map[key]
+            except ValueError:
+                continue
+        
+        str_line = str_line.replace(word, str(number), 1)
+        numbers_replaced += 1
+
+    return str_line
+
+# format words in the string to numbers and combine the first digit and the last digit to form a single two-digit number and return the sum.
 def part_two(lines):
     total = 0 
     for line in lines:
-        format_str_one = find_index(line, False)
-        format_str_two = find_index(format_str_one, True)
-        number = find_first_number(format_str_two)
-        number += find_last_number(format_str_two)
-        # testing 
-        print(f'original: {line}, formatted: {format_str_two}, number: {number}')
+        formatted_str = replace_words_for_digits(line)
+        number = find_first_number(formatted_str)
+        number += find_last_number(formatted_str)
         total += int(number)
     return total 
 
-my_text = read_file('text.txt')
+my_text = read_file_return_list('text.txt')
 
 answer_part_two = part_two(my_text)
 
