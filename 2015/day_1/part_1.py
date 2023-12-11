487dfa7c0d330141e644117ee57a8472ce6ac46e
FaulknerPearce <awesomeaustin1998@yahoo.com.au>
2023-12-11 11:14:21 -0700

    refactored code and renamed files

---
+++ 2015/day_1/part_1.py
@@ -0,0 +1,24 @@
+# covert the txtfile to a readable string of directions.
+def read_file(file):
+    with open(file, encoding='utf-8') as file:
+        text = file.read()
+        return text
+
+# Read the dicrections and returns the floor number and the basement index.
+def floor_count(my_string):
+    floor = 0
+
+    for char in my_string:
+        if char == "(":
+            floor += 1
+        else:
+            floor -= 1
+
+    return floor
+
+# ________Main Program_________ #
+my_input = read_file('text.txt')
+
+floors = floor_count(my_input)
+
+print(f'The answer to part ome is: {floors}.')
--- 2015/day_1/part_2.py
+++ 2015/day_1/part_2.py
@@ -1,16 +1,12 @@
-'''Day one of advent of code: Solution.'''
 
 # covert the txtfile to a readable string of directions.
 def read_file():
-    '''This function converts a text file to a string.'''
     with open('text.txt', encoding='utf-8') as file:
         text = file.read()
         return text
 
-# Read the dicrections and returns the floor number and the basement index.
-def floor_count(my_string):
-    '''This will return the floor number santa must go to and the index of the list that brings santa to the basement.'''
-
+# This will return the floor number santa must go to and the index of the list that brings santa to the basement..
+def basement_position(my_string):
     floor = 0
     position = 0
     breaker = 0
@@ -26,12 +22,13 @@ def floor_count(my_string):
         if floor == -1 and breaker != 1:
             basement_position = position
             breaker += 1
+            break
 
-    return floor, basement_position
+    return basement_position
 
 # ________Main Program_________ #
-my_instructions = read_file()
+my_input = read_file()
 
-floors, basement = floor_count(my_instructions)
+basement = basement_position(my_input)
 
-print(f'Santans Floor: {floors}\nBasement position: {basement}')
+print(f'The answer to part two is: {basement}.')
