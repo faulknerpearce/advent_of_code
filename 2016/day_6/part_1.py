d098d6f7a1085381b2e340eb219ffece2e6e742f
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-01-02 14:24:08 -0700

    added commets for the functions

--- 2016/day_6/part_1.py
+++ 2016/day_6/part_1.py
@@ -1,25 +1,27 @@
+# Reads a file and returns its content as a list of strings split by whitespace.
 def read_file_return_list(file):
     with open(file) as data:
         lines = [line for line in data.read().split()]
     return lines
 
+# Retrieves the letter with the maximum or minimum frequency from a dictionary.
 def get_letters(dictionary, get_maximum):
    if get_maximum:
         return max(dictionary, key=dictionary.get)
    else:
         return min(dictionary, key=dictionary.get)
 
+# Constructs a corrected message from a list of strings, using either most or least common letters per column.
 def get_error_corrected_message(rows, get_maximum=True):
     message = ''
     letters = {}
 
     for i in range(len(rows[0])):
         for row in rows:
-
             if letters.get(row[i]):
-                letters[row[i]] += 1
+                letters[row[i]] += 1 
             else:
-                letters.update({row[i]: 1})   
+                letters.update({row[i]: 1})
 
         message += get_letters(letters, get_maximum)
         letters = {}
