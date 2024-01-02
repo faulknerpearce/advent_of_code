63702cc47c5d642bb0724ad53c183c761ecf5b5d
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-01-02 14:16:22 -0700

    solution for part 2

--- 2016/day_7/part_2.py
+++ 2016/day_7/part_2.py
@@ -1,24 +1,34 @@
-from part_1 import read_file_return_list, get_outer_inner_strings
+from part_1 import read_file_return_list, get_outer_and_inner_strings
 
-# Checks if a list of strings contains a pair of characters that form a palindrome.
-def strings_have_a_pair(strlines):
-    for string in strlines:
-        for i in range(len(string) - 3):
-            characters = string[i:i+4]
-            side_a, side_b = characters[:2], characters[2:]
-            if side_a == side_b[::-1] and side_a[0] != side_a[1]:
-                return True
+# Determines if any outer string contains a three-character sequence with a corresponding reversed sequence in inner strings.
+def has_three_character_sequence(outer_strings, inner_strings):
+    for string in outer_strings:
+        for i in range(len(string) - 2):
+            characters = string[i:i+3]
+
+            if characters[0] == characters[2] and characters[0] != characters[1]:
+                if has_reversed_sequence(characters, inner_strings):
+                    return True
     return False
 
-# Counts lines that have a valid pair in outer strings but not in inner strings.
-def part_one(strlines):
+# Checks if the reversed pattern of a three-character sequence exists in any of the inner strings.
+def has_reversed_sequence(pattern, inner_strings):
+    inverse = pattern[1] + pattern[0] + pattern[1]
+
+    for string in inner_strings:
+        if inverse in string:
+            return True
+    return False
+
+# Counts the number of strings that meet the condition of having a valid three-character sequence.
+def part_two(strlines):
     count = 0
     for string in strlines:
-        outer_strings, inner_strings = get_outer_inner_strings(string)
-
-        if strings_have_a_pair(outer_strings) and not strings_have_a_pair(inner_strings):
+        outer_strings, inner_strings = get_outer_and_inner_strings(string)
+        
+        if has_three_character_sequence(outer_strings, inner_strings):
             count += 1
-
+    
     return count
 
 #________Main Program_________ #
@@ -26,6 +36,6 @@ if __name__ == "__main__":
 
     puzzle_input = read_file_return_list('text.txt')
 
-    answer = part_one(puzzle_input)
-
-    print(f'The answer to part one is: {answer}')
+    answer = part_two(puzzle_input)
+    
+    print(f'The answer to part two is: {answer}')
