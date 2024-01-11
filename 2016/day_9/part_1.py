41a1c1f4af86918a43f22ff93d36810313d8df17
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-01-11 16:52:52 -0700

    removed unneeded function

--- 2016/day_9/part_1.py
+++ 2016/day_9/part_1.py
@@ -4,6 +4,7 @@ import re
 def read_file_return_string(file):
     with open(file) as data:
         the_string = data.read()
+    
     return the_string
 
 # Returns true if a marker contains an opened and closed parenthesis in that order.
@@ -31,15 +32,6 @@ def unpack_marker(marker):
     
     return int(values[0]), int(values[1])
 
-# Checks if the remaining length of the string is sufficient for the decompression process.
-def has_sufficient_length(length_required, right_edge, the_string):
-    remaining_length = len(the_string) - (right_edge + 1)
-
-    if remaining_length == 0:
-        return False
-    else:
-        return int(length_required) <= int(remaining_length)
-
 # Generates a string by repeating a specified segment of the provided string.
 def get_repeating_characters(the_string, marker_right_edge, required_length, amount):
     repeating = ''
@@ -59,16 +51,15 @@ def decompress_string(the_string):
             
             marker_left_edge, marker_right_edge = get_marker_indices(the_string, current_index)
             decompressed_str += (the_string[current_index:marker_left_edge])
+            
             marker = the_string[marker_left_edge:marker_right_edge + 1]
             pattern_length, increase_by = unpack_marker(marker)
 
-            if has_sufficient_length(pattern_length, marker_right_edge, the_string):
-                
-                decompressed_characters = get_repeating_characters(the_string, marker_right_edge, pattern_length, increase_by)
-                decompressed_str += (decompressed_characters)
-                current_index = marker_right_edge + pattern_length + 1
-            else:
-                current_index = marker_right_edge + 1
+            decompressed_characters = get_repeating_characters(the_string, marker_right_edge, pattern_length, increase_by)
+            decompressed_str += (decompressed_characters)
+            
+            current_index = marker_right_edge + pattern_length + 1
+            
         else:
             decompressed_str += the_string[current_index:]
             break
@@ -90,4 +81,3 @@ if __name__ == "__main__":
     answer = part_one(puzzle_input)
 
     print(f'The answer to part one is: {answer}')
-   
