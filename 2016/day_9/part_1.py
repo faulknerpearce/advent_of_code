71b343b26d54c3cc0c83e643be9e17bde21ec9e3
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-01-11 15:11:17 -0700

    changed the script to read from a single string instead of an array

--- 2016/day_9/part_1.py
+++ 2016/day_9/part_1.py
@@ -1,25 +1,25 @@
 import re
 
-# Reads from a file and returns its content as a list of stripped lines.
-def read_file_return_list(file):
+# Reads from a file and returns its content as a string.
+def read_file_return_string(file):
     with open(file) as data:
-        lines = [line.strip() for line in data.readlines()]
-    return lines
+        the_string = data.read()
+    return the_string
 
 # Returns true if a marker contains an opened and closed parenthesis in that order.
-def has_marker(arr):
-   return bool(re.search(r'\([^\)]+\)', arr))
+def has_marker(the_string):
+   return bool(re.search(r'\([^\)]+\)', the_string))
 
 # Returns the indices of the opening and closing parentheses of the first marker in the string.
-def get_marker_indices(arr, start=0):
+def get_marker_indices(the_string, start=0):
     found_left = False
 
-    for i in range(start, len(arr)): 
-        if arr[i] == '(':
+    for i in range(start, len(the_string)): 
+        if the_string[i] == '(':
             found_left = True
             start_idx = i
 
-        elif arr[i] == ')' and found_left:
+        elif the_string[i] == ')' and found_left:
             end_idx = i
             break
 
@@ -32,8 +32,8 @@ def unpack_marker(marker):
     return int(values[0]), int(values[1])
 
 # Checks if the remaining length of the string is sufficient for the decompression process.
-def has_sufficient_length(length_required, right_edge, arr):
-    remaining_length = len(arr) - (right_edge + 1)
+def has_sufficient_length(length_required, right_edge, the_string):
+    remaining_length = len(the_string) - (right_edge + 1)
 
     if remaining_length == 0:
         return False
@@ -41,55 +41,54 @@ def has_sufficient_length(length_required, right_edge, arr):
         return int(length_required) <= int(remaining_length)
 
 # Generates a string by repeating a specified segment of the provided string.
-def get_repeating_characters(line, marker_right_edge, required_length, amount):
+def get_repeating_characters(the_string, marker_right_edge, required_length, amount):
     repeating = ''
     for _ in range(amount):
-        repeating += (line[marker_right_edge + 1:marker_right_edge+required_length + 1])
+        repeating += (the_string[marker_right_edge + 1:marker_right_edge+required_length + 1])
 
     return repeating
 
 # Decompresses a string according to markers that define how to expand certain segments. 
-def decompress_string(arr):
+def decompress_string(the_string):
     decompressed_str = ''
     current_index = 0
 
-    while current_index < len(arr):
+    while current_index < len(the_string):
         
-        if has_marker(arr[current_index:]):
+        if has_marker(the_string[current_index:]):
             
-            marker_left_edge, marker_right_edge = get_marker_indices(arr, current_index)
-            decompressed_str += (arr[current_index:marker_left_edge])
-            marker = arr[marker_left_edge:marker_right_edge + 1]
+            marker_left_edge, marker_right_edge = get_marker_indices(the_string, current_index)
+            decompressed_str += (the_string[current_index:marker_left_edge])
+            marker = the_string[marker_left_edge:marker_right_edge + 1]
             pattern_length, increase_by = unpack_marker(marker)
 
-            if has_sufficient_length(pattern_length, marker_right_edge, arr):
+            if has_sufficient_length(pattern_length, marker_right_edge, the_string):
                 
-                decompressed_characters = get_repeating_characters(arr, marker_right_edge, pattern_length, increase_by)
+                decompressed_characters = get_repeating_characters(the_string, marker_right_edge, pattern_length, increase_by)
                 decompressed_str += (decompressed_characters)
                 current_index = marker_right_edge + pattern_length + 1
             else:
                 current_index = marker_right_edge + 1
         else:
-            decompressed_str += arr[current_index:]
+            decompressed_str += the_string[current_index:]
             break
 
     return decompressed_str
 
 # Calculates the total length of decompressed strings from an array of strings 
-def part_one(lines):
+def part_one(string):
     decompressed_length = 0
     
-    for line in lines:
-        decompressed_string = decompress_string(line)
+    decompressed_string = decompress_string(string)
 
-        decompressed_length += len(decompressed_string)
+    decompressed_length += len(decompressed_string)
 
     return decompressed_length
 
 #________Main Program_________ # 
 if __name__ == "__main__":
     
-    puzzle_input = read_file_return_list('text.txt')
+    puzzle_input = read_file_return_string('text.txt')
 
     answer = part_one(puzzle_input)
 
