5ceb125716b331e151c408089cf400a390086c08
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-05-30 13:58:56 -0600

    refactored script

--- 2016/day_8/part_2.py
+++ 2016/day_8/part_2.py
@@ -1,43 +1,21 @@
-from part_1 import read_file_return_2D_list, create_matrix, turn_pixels_on, shift_row, shift_column, get_elements
+from part_1 import read_file_return_2D_list, generate_martix, shift_down, shift_accross, get_pixels_from_column, adjust_pixels
 
-# Prints the elements of a matrix, formatting them as a grid.
+# Outputs every pixel in each row to the terminal.
 def print_screen(matrix):
-     string = ''
-     for i in range(len(matrix)):
-          for j in range(len(matrix[i])):
-               string += matrix[i][j]
-          print(string)
-          string = ''
+    for row in matrix:
+        pixels = ''
 
-# Processes a list of instructions to manipulate the pixels on a screen (matrix).
-def part_two(instructions, screen):
-     for instruction in instructions:
+        for pixel in row:
+            pixels += pixel
+        
+        print(pixels)
 
-          if instruction[0] == 'rect':
-               start_col = int(instruction[1])
-               start_row = int(instruction[2])
-               
-               screen = turn_pixels_on(int(start_col), int(start_row), screen)
-          elif instruction[0] == 'rotate':
-               if instruction[1] == 'row':
-                    swich_row = int(instruction[2])
-                    swich_by = int(instruction[3])
+if __name__ == '__main__':
 
-                    screen = shift_row(swich_row, swich_by, screen)
-               else: 
-                    swich_row = int(instruction[2])
-                    swich_by = int(instruction[3])
+    my_instructions = read_file_return_2D_list('text.txt')
 
-                    screen = shift_column(swich_row, swich_by, screen)
-     return screen
+    my_screen = generate_martix(50, 6)
 
-#________Main Program_________ #
-if __name__ == "__main__":
-
-    puzzle_input = read_file_return_2D_list('text.txt')
-
-    screen = create_matrix(50, 6)
-
-    adjusted_screen = part_two(puzzle_input, screen)
+    adjusted_screen = adjust_pixels(my_instructions, my_screen)
 
     print_screen(adjusted_screen)
