61d954c19b363eba5467ef56e90f94892b6723ac
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-06-04 08:18:12 -0600

    refactored advent of code 2016 day 1 part 2

--- 2016/day_1/part_2.py
+++ 2016/day_1/part_2.py
@@ -1,30 +1,30 @@
 from part_1 import Compass, read_file_return_list
 
 # Processes directions to find the first location visited twice and returns its Manhattan distance from the origin
-def part_two(directions, compass, x=0, y=0):
-    seen = set()
-    seen.add((x, y))
+def part_two(directions, compass):
+    x = 0 
+    y = 0
+    visited = set()
+    visited.add((x, y))
 
     for direction in directions:
         compass.rotate(direction[0])
-        position = compass.return_position()
         steps = int(direction[1:])
         
         for _ in range(steps):
-            if position == 'N':
+            if compass.current_point == 'N':
                 y += 1
-            elif position == 'S':
+            elif compass.current_point == 'S':
                 y -= 1
-            elif position == 'E':
+            elif compass.current_point == 'E':
                 x += 1
-            elif position == 'W':
+            elif compass.current_point == 'W':
                 x -= 1
 
-            if (x, y) in seen:
+            if (x, y) in visited:
                 return abs(x) + abs(y)
            
-            seen.add((x, y))
-            print(seen)
+            visited.add((x, y))
 
 #________Main Program_________ # 
 if __name__ == "__main__":
