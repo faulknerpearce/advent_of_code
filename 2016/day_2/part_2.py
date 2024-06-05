66e3f3e35051820e6f9106412f5b3bb1b80d9a14
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-06-05 08:38:02 -0600

    refactored 2016 day 2 part 2

--- 2016/day_2/part_2.py
+++ 2016/day_2/part_2.py
@@ -1,49 +1,43 @@
-from part_1 import read_file_return_list
+from part_1 import read_file_return_list, get_row_and_col_indexes, get_digits
 
-# Finds the current position's row and column index in a 2D array.
-def get_row_col_index(array, current_character):
+# Finds the row and column indexes of the current digit in a 2D array.
+def get_row_and_col_indexes(current_digit, array):
     for row in range(len(array)):
-        if current_character in array[row]:
-            col_index = array[row].index(current_character)
-            row_index = row
-            break
-    return row_index, col_index
+        if current_digit in array[row]:
+            col =  array[row].index(current_digit)
+            return row, col 
 
 # Determines the digit on the keypad after following a sequence of directions.
-def part_two(line, array, position):
-    max_index = len(array) - 1
-    row_index, col_index = get_row_col_index(array, position)
+def get_single_digit(current_digit, instruction, array):
+    row_index, col_index = get_row_and_col_indexes(current_digit, array)
     
-    for letter in line:
+    for letter in instruction:
         if letter == 'U' and row_index > 0:
-            check_row = row_index - 1
-            if array[check_row][col_index] != 0:
+            if array[row_index -1][col_index] != 0:
                 row_index -= 1 
           
-        elif letter == 'D' and row_index < max_index:
-            check_row = row_index + 1
-            if array[check_row][col_index] != 0:
+        elif letter == 'D' and row_index < len(array) -1:
+            if array[row_index +1][col_index] != 0:
                 row_index += 1 
       
         elif letter == 'L' and col_index > 0:
-            check_col = col_index - 1
-            if array[row_index][check_col] != 0:
+            if array[row_index][col_index -1] != 0:
                 col_index -= 1 
           
-        elif letter == 'R' and col_index < max_index:
-            check_col = col_index + 1
-            if array[row_index][check_col] != 0:
+        elif letter == 'R' and col_index < len(array) -1:
+            if array[row_index][col_index +1] != 0:
                 col_index += 1   
         
     return array[row_index][col_index]
 
-# Calculates a sequence of digits based on the directions provided in each line.
-def get_characters(lines, array, value=5):
+# Returns the resulting password after following the directions provided.
+def get_digits(current_digit, instructions, array):
     digits = ''
-    for line in lines:
-       value = part_two(line, array, value)
-       digits += str(value)
-    
+
+    for instruction in instructions:
+        current_digit = get_single_digit(current_digit, instruction, array)
+        digits += str(current_digit)
+
     return digits
 
 #________Main Program_________ # 
@@ -51,8 +45,11 @@ if __name__ == "__main__":
     
     keypad_two = [[0, 0, 1, 0, 0], [0, 2, 3, 4, 0], [5, 6, 7, 8, 9], [0, 'A', 'B', 'C', 0], [0, 0, 'D', 0, 0]]
 
-    puzzle_input = read_file_return_list('text.txt')  
+    puzzle_input = read_file_return_list('text.txt') 
 
-    answer = get_characters(puzzle_input, keypad_two)  
+    default_key = 5 
+
+    answer = get_digits(default_key, puzzle_input, keypad_two)
 
     print(f'The answer to part one is: {answer}')
+    
