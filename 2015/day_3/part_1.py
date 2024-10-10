5fa31e07b4a3a73cb33c3b5bd1ad37d28c8a72bb
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-10-10 15:03:04 -0600

    refactored day three part one and part two

--- 2015/day_3/part_1.py
+++ 2015/day_3/part_1.py
@@ -1,35 +1,41 @@
-# This will read from a text file and retrun the data as a string.
+# Read from a text file and retrun the data as a string.
 def read_file_return_string(file):
     with open(file, encoding="utf-8") as file:
         text = file.read()
     return text
 
-# This will calculate the total steps of santa and the robot.
-def deliver_presents_with_robot(directions):
-    x, y = 0, 0
+# Moves the current position based on the given instruction.
+def move(x, y, instruction):
+
+    if instruction == '^':
+        x += 1
+    elif instruction == 'v':
+        x -= 1
+    elif instruction == '>':
+        y += 1
+    else:
+        y -= 1
+
+    return x, y  
+
+# Calculate the total houses that sanata visited.
+def deliver_presents(instructions):
     visited_houses = set()
     visited_houses.add((0, 0))
+    x, y = 0, 0
 
-    for direction in directions :
+    for instruction in instructions :
 
-        if direction == '>':
-            x += 1
-        elif direction == '<':
-            x -= 1
-        elif direction == '^':
-            y += 1
-        elif direction == 'v':
-            y -= 1
-
+        x, y = move(x, y, instruction)
+        
         visited_houses.add((x, y))
 
     return len(visited_houses)
 
-#________Main Program_________ # 
 if __name__ == "__main__":
 
     puzzle_input = read_file_return_string('text.txt')
 
-    answer = deliver_presents_with_robot(puzzle_input)
+    answer = deliver_presents(puzzle_input)
     
     print(f'The answer to part one is: {answer}')
--- 2015/day_3/part_2.py
+++ 2015/day_3/part_2.py
@@ -1,43 +1,32 @@
-from part_1 import read_file_return_string
+from part_1 import read_file_return_string, move
 
-# This will calculate the total steps of santa and the robot.
-def deliver_presents_with_robot(directions, robot=True):
-    santa_x, santa_y, robo_x, robo_y = 0, 0, 0, 0
+# Calculate the total houses that sanata and the robot visited.
+def deliver_presents(instructions):
     visited_houses = set()
     visited_houses.add((0, 0))
+    
+    santa_x, santa_y = 0, 0
+    robot_x, robot_y = 0, 0,
 
-    for i, direction in enumerate(directions):
-        if robot and i % 2 != 0:
-            # Robo-Santa's turn (if robot is True and it's an odd step)
-            x, y = robo_x, robo_y
+    santas_turn = True
+
+    for instruction in instructions:
+        if santas_turn:
+            santa_x, santa_y = move(santa_x, santa_y, instruction)
+            visited_houses.add((santa_x, santa_y))
+            santas_turn = False
+        
         else:
-            # Santa's turn (if robot is False or it's an even step)
-            x, y = santa_x, santa_y
-
-        if direction == '>':
-            x += 1
-        elif direction == '<':
-            x -= 1
-        elif direction == '^':
-            y += 1
-        elif direction == 'v':
-            y -= 1
-
-        visited_houses.add((x, y))
-
-        if robot and i % 2 != 0:
-            robo_x, robo_y = x, y
-        else:
-            santa_x, santa_y = x, y
+            robot_x, robot_y = move(robot_x, robot_y, instruction)
+            visited_houses.add((robot_x, robot_y))
+            santas_turn = True
 
     return len(visited_houses)
 
-
-#________Main Program_________ # 
 if __name__ == "__main__":
 
     puzzle_input = read_file_return_string('text.txt')
 
-    answer = deliver_presents_with_robot(puzzle_input)
+    answer = deliver_presents(puzzle_input)
     
     print(f'The answer to part two is: {answer}')
