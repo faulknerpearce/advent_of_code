bccf1cb62702aa158cb6b51ccf5bd31f8839050c
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-12-01 14:07:08 -0700

    create: 2024 folder, day one folder, solution for part one and part 2

---
+++ 2024/day_1/part_1.py
@@ -0,0 +1,37 @@
+# Reads a file and returns two lists of integers split from each line.
+def read_file_return_two_lists(file):
+    left = []
+    right = []
+
+    with open(file) as data:
+        text = data.readlines()
+
+        for line in text:  
+          
+          nums = line.split()
+          left.append(int(nums[0]))
+          right.append(int(nums[1]))
+
+    return left, right
+             
+# Calculates the total of absolute differences between pairs of smallest numbers from two lists.
+def part_one(left_array, right_array):
+    total = 0
+
+    left_array.sort()
+    right_array.sort()
+
+    for i in range(len(left_array)):
+
+        total += abs(left_array[i] - right_array[i])
+
+    return total
+
+
+if __name__ == '__main__':
+
+    left_side, right_side = read_file_return_two_lists('text.txt')
+
+    answer = part_one(left_side, right_side)
+
+    print(f'The answer to part one is: {answer}')
---
+++ 2024/day_1/part_2.py
@@ -0,0 +1,29 @@
+from part_1 import read_file_return_two_lists
+
+def part_two(left_array, right_array):
+    results = {}
+    total = 0
+
+    for i in range(len(left_array)):
+
+        if results.get(left_array[i]):
+            total += results[left_array[i]]
+
+        else:
+            right_count = right_array.count(left_array[i])
+            
+            product = left_array[i] * right_count
+            
+            results.update({left_array[i]: product})
+
+            total += product
+
+    return total
+
+if __name__ == '__main__':
+    
+    left_side, right_side = read_file_return_two_lists('text.txt')
+
+    answer = part_two(left_side, right_side)
+
+    print(f'The answer to part two is: {answer}')
