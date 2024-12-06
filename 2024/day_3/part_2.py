ae611054d1387131d6fed671eb6d7c8c09a84fb6
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-12-06 11:42:00 -0700

    solution for part two

---
+++ 2024/day_3/part_2.py
@@ -0,0 +1,38 @@
+import re
+
+# Reads a file and extracts 'mul(a,b)', 'do()', and 'don't()' instructions as arrays
+def read_file_return_list(file):
+    with open(file) as text:
+
+        valid_instructions = re.findall(r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)', text.read())
+        
+        array = [re.sub(r'mul|\(|\)|\'', '', line).split(',') for line in valid_instructions]
+
+    return array
+
+# Processes instructions and calculates the total based on enable/disable commands.
+def part_two(instructions):
+    enable_mul = True
+    total = 0
+
+    for instruction in instructions:
+
+        if instruction[0].isdigit() and enable_mul:  
+            product = int(instruction[0]) * int(instruction[1])
+            total += product
+
+        elif instruction[0] == 'dont':
+            enable_mul = False
+
+        elif instruction[0] == 'do':
+            enable_mul = True
+
+    return total
+
+if __name__ == '__main__':
+
+    puzzle_input = read_file_return_list('text.txt')
+
+    answer = part_two(puzzle_input)
+
+    print(f'The answer to part two is: {answer}')
