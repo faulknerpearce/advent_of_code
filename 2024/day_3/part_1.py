41d016f5e1f57b3ad938e35604c0126207b73f78
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-12-06 08:40:35 -0700

    solution for day three part one

---
+++ 2024/day_3/part_1.py
@@ -0,0 +1,24 @@
+import re 
+
+# Reads a file and extracts valid 'mul(a,b)' instructions into a list of number pairs.
+def read_file_return_list(file):
+    with open(file) as text:
+
+        valid_instructions = re.findall(r'mul\(\d+,\d+\)', text.read())
+        
+        array = [re.sub(r'mul|\(|\)', '', line).split(',') for line in valid_instructions]
+
+    return array
+
+# Computes the sum of the product of number pairs from the instructions.
+def part_one(instructions):
+    
+    return sum(map(lambda instruction: int(instruction[0]) * int(instruction[1]), instructions))
+
+if __name__ == '__main__':
+
+    puzzle_input = read_file_return_list('text.txt')
+
+    answer = part_one(puzzle_input)
+
+    print(f'The answer to part one is: {answer}')
