0d8e7131071684f7dac1dd792f84ff699329d478
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-12-07 09:11:04 -0700

    refactored day three part one and two

--- 2016/day_3/part_1.py
+++ 2016/day_3/part_1.py
@@ -1,25 +1,27 @@
-# Reads a file and returns a 2D array of numbers.
-def read_file_return_2D_list(file, lines=[]):
-    with open(file) as data:
-        for line in data:
-            nums = [int(num) for num in line.split()]
-            lines.append(nums)
-    return lines
+# Reads a file and returns its content as a 2D list.
+def read_file_return_2D_list(file):
+    with open(file) as text:
+        instructions = [line.split() for line in text.readlines()]
+    return instructions
 
-# Counts the number of valid triangles by row in an arry.
-def get_possible_triangle_rows(lines):
-    count = 0
-    for line in lines:
-        a, b, c = line[0], line[1], line[2]
-        if (a + b > c) and (a + c > b) and (b + c > a):
-            count += 1
-    return count
+# Checks if three given sides can form a valid triangle.
+def is_triangle(a, b, c):
+    return (int(a) + int(b)) > int(c) and (int(b) + int(c)) > int(a) and (int(c) + int(a)) > int(b)
 
-#________Main Program_________ # 
-if __name__ == "__main__":
+# Counts the number of valid triangles in the given instructions, where each row represents a triangle.
+def count_valid_triangles_by_row(instructions):
+    total = 0 
 
+    for instruction in instructions:
+        if is_triangle(instruction[0], instruction[1], instruction[2]):
+            total += 1
+    return total 
+
+if __name__ == '__main__':
+    
     puzzle_input = read_file_return_2D_list('text.txt')
 
-    answer = get_possible_triangle_rows(puzzle_input)
+    answer = count_valid_triangles_by_row(puzzle_input)
 
     print(f'The answer to part one is: {answer}')
+    
--- 2016/day_3/part_2.py
+++ 2016/day_3/part_2.py
@@ -1,26 +1,24 @@
-from part_1 import read_file_return_2D_list
+from part_1 import read_file_return_2D_list, is_triangle
 
-# Counts the number of valid triangles formed by vertical columns in three rows
-def count_triangles(row_one, row_two, row_three):
-    count = 0
-    for i in range(3):
-        if (row_one[i] + row_two[i] > row_three[i]) and (row_one[i] + row_three[i] > row_two[i]) and (row_two[i] + row_three[i] > row_one[i]):
-            count += 1
-    return count
+# Counts the number of valid triangles in the given instructions by columns, where each set of three rows forms a triangle.
+def count_valid_triangles_by_column(instructions):
+    total = 0
 
-# Calculates the total count of valid triangles when considering columns of three consecutive rows
-def get_possible_triangle_columns(lines):
-    valid_count = 0
-    for row in range(0, len(lines), 3):
-        row_a, row_b, row_c = lines[row], lines[row+1], lines[row+2]
-        valid_count += count_triangles(row_a, row_b, row_c)  
-    return valid_count 
+    for col in range(3):
+        for row in range(0, len(instructions), 3):
 
-#________Main Program_________ # 
-if __name__ == "__main__":
+            a = instructions[row][col]
+            b = instructions[row+1][col]
+            c = instructions[row+2][col]
+
+            if is_triangle(a, b, c):
+                total += 1
+    return total
+
+if __name__ == '__main__':
 
     puzzle_input = read_file_return_2D_list('text.txt')
 
-    answer = get_possible_triangle_columns(puzzle_input)
+    answer = count_valid_triangles_by_column(puzzle_input)
 
     print(f'The answer to part two is: {answer}')
