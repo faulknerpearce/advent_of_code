0a59370b0dc9cccc7d68a8e65daca8fdc1abb0a6
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-12-12 08:55:30 -0700

    added the event link

--- 2024/day_5/part_1.py
+++ 2024/day_5/part_1.py
@@ -73,7 +73,7 @@ def part_one(updates, rules_map):
 
     return total 
 
- # Event: https://adventofcode.com/2024/day/5
+# Event: https://adventofcode.com/2024/day/5
 if __name__ == '__main__':
 
     puzzle_input = read_file_return_3d_array('text.txt')
