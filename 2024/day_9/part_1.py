fcd2bec2b501a36f8c8376347b5d342d6dc8af4d
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-12-16 11:11:29 -0700

    refatored day nine part one, optimized file search algorithm.

--- 2024/day_9/part_1.py
+++ 2024/day_9/part_1.py
@@ -1,9 +1,11 @@
+# Reads the input file and returns the content as a string.
 def read_file_return_string(file):
     with open(file) as data:
         string = data.read().strip('\n')
 
     return string
-    
+
+# Unpacks the dense disk map into a list of file blocks and free spaces.  
 def unpack_disc(disk_map):
     disk = []
     for i in range(len(disk_map)):
@@ -14,41 +16,43 @@ def unpack_disc(disk_map):
 
     return disk
 
-def has_space(disk):
-    total_spaces = 0
-    spaces = 0
+# Creates a dictionary mapping file block indices to their IDs.
+def create_file_dict(disk):
+    file_dict = {}
+    end = len(disk)
 
-    for i in range(len(disk) -1):
-        if disk[i] == '.':
-            spaces += 1
+    for i in range(len(disk)):
 
-        if disk[i].isdigit():
-            total_spaces +=  spaces
-            spaces = 0
+        if disk[-i-1].isdigit():
+                file_dict.update({end-i-1: disk[-i-1]})
 
-    return total_spaces
+    return file_dict
 
-def get_leftmost(disk):
+# Finds the index of the leftmost free space.
+def get_left_index(disk):
     for j in range(len(disk)):
         if disk[j] == '.':
             return j
-
-def get_rightmost(disk):
-    end = len(disk) -1
-
-    for i in range(end):
-        if disk[end-i].isdigit():
-            return end-i
         
-def compact_disk(disk):
-    spaces = has_space(disk)
+    return None
 
-    for i in range(spaces):
+# Retrieves the index of the rightmost file block and removes it from the dictionary.
+def get_right_index(file_dict):
+    for key in file_dict.keys():
+        del file_dict[key]
 
-        left_idx = get_leftmost(disk)
-        right_idx = get_rightmost(disk)
+        return key
+    
+    return None
 
-        if left_idx < right_idx:
+# Rearranges file blocks to the leftmost free space.  
+def compact_disk(disk, file_dict):
+
+    for _ in range(len(file_dict)):
+        left_idx = get_left_index(disk)
+        right_idx = get_right_index(file_dict)
+
+        if left_idx is not None and right_idx is not None and left_idx < right_idx:
             disk[left_idx], disk[right_idx] = disk[right_idx], disk[left_idx]
             
         else:
@@ -56,6 +60,7 @@ def compact_disk(disk):
 
     return disk
 
+# Calculates the checksum as the sum of ( index * file_id) for all file blocks.
 def get_checksum(disk):
     total = 0
     
@@ -64,15 +69,19 @@ def get_checksum(disk):
             total += i * int(disk[i])
 
     return total
-         
+
+# Event: https://adventofcode.com/2024/day/9        
 if __name__ == '__main__':
 
     puzzle_input = read_file_return_string('text.txt')
 
     disk = unpack_disc(puzzle_input)
 
-    compacted_disk = compact_disk(disk)
+    file_dictionary = create_file_dict(disk)
+
+    compacted_disk = compact_disk(disk, file_dictionary)
 
     answer = get_checksum(compacted_disk)
 
     print(f'the answer to part one is: {answer}')
+    
