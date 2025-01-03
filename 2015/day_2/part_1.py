c8aa46eff7014def496010e292d15c40102ec922
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-01-02 17:20:10 -0700

    added doc strings to all functions for  day two part one

--- 2015/day_2/part_1.py
+++ 2015/day_2/part_1.py
@@ -1,19 +1,19 @@
 import re 
 
-# Reads the file and returns a 2D list where each sublist contains the dimensions of a box.
 def read_file_return_2d_list(file):
+    '''Reads the file and returns a 2D list where each sublist contains the dimensions of a box.'''
     with open(file) as text:
         formatted = re.sub(r'x', ' ', text.read())
         instructions = [line.split() for line in formatted.split('\n')]
         
         return instructions
-
-# Calculates the slack (smallest side area) of the box.   
+   
 def get_slack(l, w, h):
+    '''Calculates the slack (smallest side area) of the box.'''
     return min((l * w), (l * h), (w * h))
 
-# Calculates the total wrapping paper needed for a single box, including the slack.
 def calculate_dimensions(instruction):
+    '''Calculates the total wrapping paper needed for a single box, including the slack.'''
     length, width, height = int(instruction[0]), int(instruction[1]), int(instruction[2])
 
     box = (2 * length * width) + (2 * length * height) + (2 * width * height) 
@@ -22,8 +22,8 @@ def calculate_dimensions(instruction):
     
     return box + slack
 
-# Sums up the total wrapping paper needed for all boxes.
-def calculate_total_wrapping_papper(instructions):
+def calculate_total_wrapping_paper(instructions):
+    '''Sums up the total wrapping paper needed for all boxes.'''
     total = 0
 
     for instruction in instructions:
@@ -31,10 +31,11 @@ def calculate_total_wrapping_papper(instructions):
 
     return total
 
+# Event: https://adventofcode.com/2015/day/2
 if __name__ == '__main__':
 
     puzzle_input = read_file_return_2d_list('text.txt')
 
-    answer = calculate_total_wrapping_papper(puzzle_input)
+    answer = calculate_total_wrapping_paper(puzzle_input)
 
     print(f'The answer to part one is: {answer}')
