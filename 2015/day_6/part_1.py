60eda3b735e66ad90b394e9fcc190b0112095719
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-01-06 08:06:41 -0700

    included doc strings to all functions for day six part one

--- 2015/day_6/part_1.py
+++ 2015/day_6/part_1.py
@@ -1,26 +1,26 @@
 import re
 
-# This will read and format a text file and return a 2d list of instructions.
 def read_file_return_2d_list(file):
+    '''This will read and format a text file and return a 2d list of instructions.'''
     with open(file) as text:
         formatted = re.sub(r'turn|through|,', ' ', text.read())
 
         return [ line.split() for line in formatted.split('\n') ]
 
-# This will create a two-dimensional list.
 def create_matrix(size):
+    '''This will create a two-dimensional list.'''
     return [[ 0 for _ in range(size) ] for _ in range(size) ]
 
-# Set the specified rectangular range of lights to the 'on' or 'off' state in the grid.
 def set_lights(power, start_row, end_row, start_col, end_col, matrix):
+    '''Set the specified rectangular range of lights to the 'on' or 'off' state in the grid.'''
     for row in range(start_row, end_row + 1):
         for col in range(start_col, end_col + 1):
             matrix[row][col] = power
 
     return matrix
 
-# Toggle the specified rectangular range of lights.
 def toggle_lights(start_row, end_row, start_col, end_col, matrix):
+    '''Toggle the specified rectangular range of lights.'''
     for row in range(start_row, end_row + 1):
         for col in range(start_col, end_col + 1):
             if matrix[row][col] == 1:
@@ -30,8 +30,8 @@ def toggle_lights(start_row, end_row, start_col, end_col, matrix):
 
     return matrix
 
-# Count the number of lights that are turned on.
 def count_lights(matrix):
+    '''Count the number of lights that are turned on.'''
     count = 0 
 
     for row in matrix:
@@ -40,8 +40,8 @@ def count_lights(matrix):
 
     return count
 
-# Follow the provided instructions and return the adjusted light grid.
 def part_one(instructions, matrix):
+    '''Follow the provided instructions and return the adjusted light grid.'''
     for instruction in instructions:
         if instruction[0] == 'on':
             matrix = set_lights(1, int(instruction[1]), int(instruction[3]), int(instruction[2]), int(instruction[4]), matrix)
@@ -52,6 +52,7 @@ def part_one(instructions, matrix):
        
     return matrix
 
+# Event: https://adventofcode.com/2015/day/6
 if __name__ == "__main__":
 
     puzzle_input = read_file_return_2d_list('text.txt')
