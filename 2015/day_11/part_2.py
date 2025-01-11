ca879e58c07a8c1b3475c8022991286e949303cb
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-01-11 08:16:52 -0700

    added doc strings to all functions for day eleven part two

--- 2015/day_11/part_2.py
+++ 2015/day_11/part_2.py
@@ -1,13 +1,14 @@
-from part_1 import shift_letters, does_not_contain, has_accending, has_two_pairs
+from part_1 import shift_letters, does_not_contain, has_ascending, has_two_pairs
 
-# Generates the next valid password.
 def part_two(password):
+    '''Generates the next valid password.'''
     while True:
         password = shift_letters(password)
 
-        if does_not_contain(password) and has_accending(password) and has_two_pairs(password):
+        if does_not_contain(password) and has_ascending(password) and has_two_pairs(password):
             return ''.join(password)
 
+# Event: https://adventofcode.com/2015/day/11
 if __name__ == '__main__':
 
     puzzle_input = ['h', 'x', 'b', 'x', 'x', 'y', 'z', 'z']
