583be21abf98f5dfe97de459392021a510021260
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-01-11 08:35:27 -0700

    refacroted the bfs function

--- 2024/day_16/part_1.py
+++ 2024/day_16/part_1.py
@@ -23,7 +23,6 @@ def eligible_position(score, row, col, direction, visited):
 def breath_first_search(start_row, start_col, matrix):
     '''Uses BFS with a priority queue to find the minimum score path from 'S' to 'E'.'''
     directions = {'N': (-1, 0), 'E': (0, 1), 'S': (1, 0), 'W': (0, -1)}
-    directions_list = ['N', 'E', 'S', 'W']
     queue = [(0, start_row, start_col, 'E')]  
     visited = {}
 
@@ -40,13 +39,14 @@ def breath_first_search(start_row, start_col, matrix):
             visited[(new_row, new_col, cur_direction)] = cur_score + 1
             heapq.heappush(queue, (cur_score + 1, new_row, new_col, cur_direction))
 
-        for direction in directions_list:
+        for direction in directions.keys():
             if direction != cur_direction and eligible_position(cur_score + 1000, cur_row, cur_col, direction, visited):
                 visited[(cur_row, cur_col, direction)] = cur_score + 1000
                 heapq.heappush(queue, (cur_score + 1000, cur_row, cur_col, direction))
 
     return None
 
+# Event: https://adventofcode.com/2024/day/16
 if __name__ == '__main__':
     puzzle_input = read_file_return_2d_list('text.txt')
 
